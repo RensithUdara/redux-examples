29b5ee06db35857a6f958121fe885865df0490e5
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-14 00:37:12 +0530

    create fruitReducer function

--- src/main.js
+++ src/main.js
@@ -9,8 +9,19 @@ const store = {
   // PharmacyItems : ["P1" , "P2" , "P3"],
 };
 
+function fruitReducer(action) {
+switch (action) {
+  case "BUY_FRUIT" :
+    store.noOfFruits = store.noOfFruits - 1;
+    break;
+    default:
+    break;
+}
+}
+
 console.log(store);
-console.log("BUY_FRUIT");
+//console.log("BUY_FRUIT");
 
 store.noOfFruits = store.noOfFruits - 1;
+fruitReducer ("BUY_FRUIT");
 console.log(store);
