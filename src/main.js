77896a14cae3ef32496d2e6bbd3aeb16acfd76a3
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-14 00:32:21 +0530

    add console log

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // Keels Super
 
 const store = {
-  
+
   noOfFruits: 5,
   noOfPharmacyItems: 10,
 
   // fruits : ["Apple" , "Orange" , "Bananna"],
   // PharmacyItems : ["P1" , "P2" , "P3"],
-}
+};
+
+console.log(store);
